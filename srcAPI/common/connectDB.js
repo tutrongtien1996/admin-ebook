56e3b2b56e14796285311b3b04de0e85c19ba143
tutrongtien1996 <101469169+tutrongtien1996@users.noreply.github.com>
2023-03-17 15:46:32 +0700

    update config

--- srcAPI/common/connectDB.js
+++ srcAPI/common/connectDB.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv').config();
 const db = require('knex')({
   client: 'mysql',
   connection: {
-    host: dotenv.parsed.MY_HOST,
-    port: dotenv.parsed.MY_PORT,
-    user: dotenv.parsed.MY_USER,
-    password: dotenv.parsed.MY_PASSWORD,
-    database: dotenv.parsed.MY_DATABASE
+    host: dotenv.parsed.MYSQL_HOST,
+    port: dotenv.parsed.MYSQL_PORT,
+    user: dotenv.parsed.MYSQL_USER,
+    password: dotenv.parsed.MYSQL_PASSWORD,
+    database: dotenv.parsed.MYSQL_DATABASE
   }
 });
 db.raw("SELECT 1").then(() => {
